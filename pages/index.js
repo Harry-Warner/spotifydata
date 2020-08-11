3743cb46d619058fb2c307eb5db43fc6f04d7b9b
Harry-Warner <harry.e.p.warner@gmail.com>
2020-08-11 22:35:22 +0100

    split data into 3 time ranges

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,37 @@
 import React from "react";
 import Router from "next/router";
 
-const Home = ({ artists }) => {
-  console.log(artists);
-  const spotifydata = artists.items;
+const Home = ({ spotifydata }) => {
+  console.log(spotifydata);
   return (
     <>
       {spotifydata ? (
-        spotifydata.map((item) => <p>{item.name}</p>)
+        <div style={{ display: "flex" }}>
+          <ul>
+            {spotifydata.short.items.map((item) => (
+              <li key={item.id}>
+                <h2>{item.name}</h2>
+                <p>Popularity: {item.popularity}</p>
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {spotifydata.medium.items.map((item) => (
+              <li key={item.id}>
+                <h2>{item.name}</h2>
+                <p>Popularity: {item.popularity}</p>
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {spotifydata.long.items.map((item) => (
+              <li key={item.id}>
+                <h2>{item.name}</h2>
+                <p>Popularity: {item.popularity}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       ) : (
         <button onClick={() => Router.push("/api/login")}>
           Sign in to spotify
@@ -18,11 +42,22 @@ const Home = ({ artists }) => {
 };
 
 Home.getInitialProps = async ({ query }) => {
-  const res = await fetch("https://api.spotify.com/v1/me/top/artists", {
+  const authHeaders = {
     headers: { Authorization: "Bearer " + query.access_token },
-  });
-  const json = await res.json();
-  return { artists: json };
+  };
+  const long = await fetch(
+    `https://api.spotify.com/v1/me/top/artists?limit=50&time_range=long_term`,
+    authHeaders
+  ).then((res) => res.json());
+  const medium = await fetch(
+    `https://api.spotify.com/v1/me/top/artists?limit=50&time_range=medium_term`,
+    authHeaders
+  ).then((res) => res.json());
+  const short = await fetch(
+    `https://api.spotify.com/v1/me/top/artists?limit=50&time_range=short_term`,
+    authHeaders
+  ).then((res) => res.json());
+  return { spotifydata: { long: long, medium: medium, short: short } };
 };
 
 export default Home;
